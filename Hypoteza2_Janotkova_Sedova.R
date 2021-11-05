458a3220ac31ce78e1f1379925590b8f4e280a44
Natalia Sedova <sedova.natalia9@gmail.com>
2021-11-05 20:41:38 +0100

    pridane grafy v pdf a trosku kodu

--- Hypoteza2_Janotkova_Sedova.R
+++ Hypoteza2_Janotkova_Sedova.R
@@ -27,6 +27,18 @@ extractedData <- map(models, ~f(.x))
 sd(map_dbl(models, ~ coef(.x)[1]))
 sd(map_dbl(models, ~ coef(.x)[2]))
 
+# vyber len koeficientov beta0 zo vsetkych lin. modelov
+beta0_all <- map_dbl(models, ~ coef(.x)[1])
+# vypocet standardnej odchylky pre koeficient beta0=intercept v ramci vsetkych podmnozin
+beta0_sd <- sd(beta0_all)
+beta0_all # z vypisu pozeram, ake boli ziskane hodnoty a ako sa menili
+
+# vyber len koeficientov beta1 zo vsetkych lin. modelov
+beta1_all <- map_dbl(models, ~ coef(.x)[2])
+# vypocet standardnej odchylky pre koeficient beta1=slope v ramci vsetkych podmnozin
+beta1_sd <- sd(beta1_all)
+beta1_all
+
 rss <- map_dbl(models, ~ sum(resid(.x)^2))
 rse <- map_dbl(rss, ~ sqrt(.x/0.4*dim(ourData)[1]-2))
 boxplot(rss)
@@ -61,3 +73,26 @@ ourData %>% arrange(FruitsAndVegetables)
 ourData %>% arrange(desc(FruitsAndVegetables))
 ourData %>% arrange(Recovered)
 ourData %>% arrange(desc(Recovered))
+
+#linearny model
+project1 <- read_csv("C:\\Users\\sedov\\Documents\\archive\\dataset.csv")
+
+project <- project1 %>% select(Country, 'Fruits - Excluding Wine', Vegetables,+
+                                 Population, Recovered) %>% drop_na()
+
+names(project)[2] <- 'Fruits'
+ourData <- project %>% mutate(FruitsAndVegetables = (Fruits+Vegetables)/2) %>% 
+  select(Country, Fruits, Vegetables, Population, Recovered, FruitsAndVegetables) %>% 
+  drop_na()
+
+fit <- lm(Recovered ~ FruitsAndVegetables, data = ourData)
+summary(fit)
+
+# vyber koeficientov
+beta0 = coef(fit)[1]
+beta1 = coef(fit)[2]
+# pomocna funkcia
+lin_fun <- function(x){beta0 + beta1*x}
+# vykreslenie lm aplikovaneho na cely dataset bez regresie
+ggplot(ourData, aes(FruitsAndVegetables, Recovered)) + geom_point(colour = 'blue') + geom_function(fun = lin_fun, colour = 'red')
+
